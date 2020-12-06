685172387a399ebac758a5cd20d6df34235bda66
paweloszu <zujewski81@gmail.com>
2020-12-06 20:17:45 +0100

    Update panel_usera.php

--- panel_usera.php
+++ panel_usera.php
@@ -2,6 +2,7 @@
 require("db.php");
 $rezultat = mysqli_query($con, "SELECT * from logi where idu = $_COOKIE[idu] ORDER BY idl DESC") or die ("Błąd zapytania do bazy danych");
 $rekord = mysqli_fetch_array($rezultat);
+$rekord = mysqli_fetch_array($rezultat);
 ?>
 <!DOCTYPE html>
 <html>
